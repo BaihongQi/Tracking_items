f49bdc1016fe78fce1c1b12b8c19fe684bedb6fa
BaihongQi <bq@ualberta.ca>
2017-05-01 16:54:02 -0600

    idenfity code number

---
+++ test.rb
@@ -0,0 +1,3 @@
+p (/[0-9a-f]/.match('9f'))
+matchdata="P001234".match(/[[:upper:]]+[[:digit:]]{6}/)
+puts matchdata[0]
--- tracking.rb
+++ tracking.rb
@@ -1,1 +1,18 @@
-require 'roo'
+require 'spreadsheet'
+book = Spreadsheet.open('Copy of Shipment 56.xls')
+sheet1 = book.worksheet('Shipping list')
+sum=0
+sheet1.each do |row|
+  if (not row[1].nil? and not row[5].nil?) or (not row[1].nil? and not row[6].nil?)
+    sum+=1
+    if not row[5].nil? and row[5].match(/[[:upper:]]+[[:digit:]]{6}/)
+      puts "No:#{sum} Name: #{row[1]} Node: #{row[5].match(/[[:upper:]]+[[:digit:]]{6}/)[0]}"
+      #puts sum, row[1],row[5]
+    elsif not row[6].nil? and row[6].match(/[[:upper:]]+[[:digit:]]{6}/)
+      puts "No:#{sum} Name: #{row[1]} Node: #{row[6].match(/[[:upper:]]+[[:digit:]]{6}/)[0]}"
+      #puts sum, row[1],row[6]
+    end
+    #puts row[1],row[5]
+  end
+  #puts row.join(',') # looks like it calls "to_s" on each cell's Value
+end
