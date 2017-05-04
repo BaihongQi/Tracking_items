3bb53c79d484d9de5fef7d542e2bb8193660c5c9
BaihongQi <bq@ualberta.ca>
2017-05-04 15:10:28 -0600

    add jeoffry code

--- tracking2.rb
+++ tracking2.rb
@@ -12,10 +12,12 @@ def mysql_query(connection,query)
 end
 
 
-CSV.open("shipment52_missing.csv","w") do |csv1|
+CSV.open("shipment52_mounted.csv","w") do |csv1|
 book = Spreadsheet.open('Shipment 52.xls')
 sheet1 = book.worksheet('Shipping list')
-sum=0
+sum=0#total
+sum2=0#not parent
+sum3=0#parent
 connection = Helpers.set_mysql_connection
 sheet1.each do |row|
   #check if p number exsit
@@ -30,45 +32,53 @@ sheet1.each do |row|
       whole=node[1,6]
       result=File.file?("/diginit/work/peel/metadata/N/#{first}/#{second}/#{node}.xml")
       if result==true
-        puts "parent"
+        sum3+=1
+        puts "parent #{sum} #{sum3}"
+        #csv1 << [row[1],row[5]]
         #save into a csv about parent items Name
       end
-    else #not parent
-      if not row[4].nil?
-        year=row[4]
-        puts "No:#{sum} Name: #{title} Year: #{year} Node: #{node}"
-      else
-        puts "No:#{sum} Name: #{title} Node: #{node}"
-      end
-
-      check_exsit="select * from items where code='#{node}' or old_peel_new='#{node}'"
-      rs1 = mysql_query(connection, check_exsit)
-      row_num1=0
-      rs1.each do |row1|
-        row_num1+=1
-      end
-      if row_num1!=0
-        check_mounted="select * from items where (code='#{node}' or old_peel_new='#{node}') and digstatus='mounted'"
-        rs2 = mysql_query(connection, check_mounted)
-        row_num2=0
-        rs2.each do |row2|
-          row_num2+=1
-          #csv1 << [row2['code'],row2['old_peel_new']]
-        end
-
-        puts "row number #{row_num1}------------------------------------------------"
-      else
-        puts "not in database"
-        csv1 << [row[1],row[5],row[6]]
-        if not row[8].nil?
-          insert="INSERT INTO items_copy(code, digstatus,scanimages) VALUES ('#{node}','shipped','#{row[8]}') ON DUPLICATE KEY UPDATE digstatus=VALUES(digstatus), scanimages=VALUES(scanimages)"
+    end
+    if not row[8].nil? or result!=true
+      sum2+=1
+        if not row[4].nil?
+          year=row[4]
+          puts "not parent #{sum} #{sum2}"
+          #puts "No:#{sum} Name: #{title} Year: #{year} Node: #{node}"
         else
-          insert="INSERT INTO items_copy(code, digstatus) VALUES ('#{node}','shipped') ON DUPLICATE KEY UPDATE digstatus=VALUES(digstatus)"
+          puts "not parent #{sum} #{sum2}"
+          #puts "No:#{sum} Name: #{title} Node: #{node}"
         end
-        mysql_query(connection, insert)
+
+        check_exsit="select * from items where code='#{node}' or old_peel_new='#{node}'"
+        #puts "#{sum2}: #{node}"
+        rs1 = mysql_query(connection, check_exsit)
+        row_num1=0
+        rs1.each do |row1|
+          row_num1+=1
+        end
+        if row_num1!=0
+          check_mounted="select * from items where (code='#{node}' or old_peel_new='#{node}') and digstatus='mounted'"
+          #check_mounted="select * from items where (code='#{node}' or old_peel_new='#{node}') and noid IS NULL"
+          rs2 = mysql_query(connection, check_mounted)
+          row_num2=0
+          rs2.each do |row2|
+            row_num2+=1
+            csv1 << [row2['code'],row2['old_peel_new']]
+          end
+
+          #puts "row number #{row_num1}------------------------------------------------"
+        else
+          #puts "not in database #{node}"
+          #csv1 << [row[1],row[5]]
+          if not row[8].nil?
+            insert="INSERT INTO items_test(code, digstatus,scanimages) VALUES ('#{node}','shipped','#{row[8]}') ON DUPLICATE KEY UPDATE digstatus=VALUES(digstatus), scanimages=VALUES(scanimages)"
+          else
+            insert="INSERT INTO items_test(code, digstatus) VALUES ('#{node}','shipped') ON DUPLICATE KEY UPDATE digstatus=VALUES(digstatus)"
+          end
+          #mysql_query(connection, insert)
+        end
       end
     end
-  end
 end
 Helpers.close_mysql_connection(connection)
 end
