f02e773df1b1387f9365109da1ece60f434a70f2
BaihongQi <bq@ualberta.ca>
2017-05-08 17:10:09 -0600

    able to generate mods records list

--- tracking2.rb
+++ tracking2.rb
@@ -1,6 +1,7 @@
 require 'spreadsheet'
 require './helpers'
 require 'csv'
+require 'optparse'
 
 def mysql_query(connection,query)
   begin
@@ -11,9 +12,22 @@ def mysql_query(connection,query)
   end
 end
 
+options = {}
+  OptionParser.new do |opts|
+    opts.on("-f", "--file-name FILE", "the files of shipping list which is going to be tracked.") do |v|
+      options[:file] = v
+    end
+  end.parse!
+file=options[:file]
+puts file
+filename=File.basename(file).split(".").first
+puts filename
 
-CSV.open("shipment52_mounted.csv","w") do |csv1|
-book = Spreadsheet.open('Shipment 52.xls')
+
+CSV.open("#{filename}_mounted.csv","w") do |csv1|
+CSV.open("#{filename}_not_in_openstack.csv","w") do |csv2|
+CSV.open("#{filename}_missing_object.csv","w") do |csv3|
+book = Spreadsheet.open(file)
 sheet1 = book.worksheet('Shipping list')
 sum=0#total
 sum2=0#not parent
@@ -58,18 +72,25 @@ sheet1.each do |row|
         end
         if row_num1!=0
           check_mounted="select * from items where (code='#{node}' or old_peel_new='#{node}') and digstatus='mounted'"
-          #check_mounted="select * from items where (code='#{node}' or old_peel_new='#{node}') and noid IS NULL"
+          check_stack="select * from items where (code='#{node}' or old_peel_new='#{node}') and noid IS NULL"
           rs2 = mysql_query(connection, check_mounted)
           row_num2=0
           rs2.each do |row2|
             row_num2+=1
-            #csv1 << [row2['code'],row2['old_peel_new']]
+            csv1 << [row2['code'],row2['old_peel_new']]
           end
+          rs3 = mysql_query(connection, check_stack)
+          row_num3=0
+          rs3.each do |row3|
+            row_num3+=1
+            csv2 << [row3['code'],row3['old_peel_new']]
+          end
+
 
           #puts "row number #{row_num1}------------------------------------------------"
         else
-          #puts "not in database #{node}"
-          #csv1 << [row[1],row[5]]
+          puts "not in database #{node}"
+          csv3 << [row[1],row[5]]
           if not row[8].nil?
             insert="INSERT INTO items(code, digstatus,scanimages) VALUES ('#{node}','shipped','#{row[8]}') ON DUPLICATE KEY UPDATE digstatus=VALUES(digstatus), scanimages=VALUES(scanimages)"
           else
@@ -81,4 +102,6 @@ sheet1.each do |row|
     end
 end
 Helpers.close_mysql_connection(connection)
+end
+end
 end
